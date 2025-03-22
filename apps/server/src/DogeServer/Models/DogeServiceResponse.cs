3c875344c30887536ca2af21a52287b5abdb5756
jacobvoller <act@jacobvoller.com>
2025-03-22 10:37:51 -0500

    REMOVE response.message - not needed

--- apps/server/src/DogeServer/Models/DogeServiceResponse.cs
+++ apps/server/src/DogeServer/Models/DogeServiceResponse.cs
@@ -5,7 +5,6 @@ namespace DogeServer.Models
     public class DogeServiceControllerResponse<T>
     {
         public T? Results { get; set; }
-        public string? Message { get; set; }
         public int? StatusCode { get; set; }
         public int? ErrorCode { get; set; }
         public string? ErrorMessage { get; set; }

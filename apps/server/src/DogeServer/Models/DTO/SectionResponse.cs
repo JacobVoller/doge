f6a5e8e85aebff73d6097763425628b45afba8f6
jacobvoller <act@jacobvoller.com>
2025-03-22 10:42:05 -0500

    LINT

--- apps/server/src/DogeServer/Models/DTO/SectionResponse.cs
+++ apps/server/src/DogeServer/Models/DTO/SectionResponse.cs
@@ -1,34 +1,33 @@
 ﻿using DogeServer.Models.Entities;
 using Newtonsoft.Json;
 
-namespace DogeServer.Models.DTO
+namespace DogeServer.Models.DTO;
+
+public class SectionResponse
 {
-    public class SectionResponse
-    {
-        [JsonProperty("content_versions")]
-        public Section[]? ContentVersions { get; set; }
-        
-        [JsonProperty("meta")]
-        public SectionMeta? Meta { get; set; }
-    }
+    [JsonProperty("content_versions")]
+    public Section[]? ContentVersions { get; set; }
+    
+    [JsonProperty("meta")]
+    public SectionMeta? Meta { get; set; }
+}
 
-    public class SectionMeta
-    {
-        [JsonProperty("title")] 
-        public string? Title;
+public class SectionMeta
+{
+    [JsonProperty("title")] 
+    public string? Title;
 
-        [JsonProperty("result_count")]
-        public string? ResultCount;
-        
-        // issue_date": {
-        //    lte: "2018-01-01",
-        //    gte: "2017-01-01"
-        // }
+    [JsonProperty("result_count")]
+    public string? ResultCount;
+    
+    // issue_date": {
+    //    lte: "2018-01-01",
+    //    gte: "2017-01-01"
+    // }
 
-        [JsonProperty("latest_amendment_date")]
-        public string? LatestAmendmentDate;
+    [JsonProperty("latest_amendment_date")]
+    public string? LatestAmendmentDate;
 
-        [JsonProperty("latest_issue_date")]
-        public string? LatestIssueDate;
-    }
+    [JsonProperty("latest_issue_date")]
+    public string? LatestIssueDate;
 }
--- apps/server/src/DogeServer/Models/DTO/TitleResponse.cs
+++ apps/server/src/DogeServer/Models/DTO/TitleResponse.cs
@@ -1,23 +1,22 @@
 ﻿using DogeServer.Models.Entities;
 using Newtonsoft.Json;
 
-namespace DogeServer.Models.DTO
+namespace DogeServer.Models.DTO;
+
+public class TitleResponse
 {
-    public class TitleResponse
-    {
-        [JsonProperty("titles")]
-        public Title[]? Titles { get; set; }
+    [JsonProperty("titles")]
+    public Title[]? Titles { get; set; }
 
-        [JsonProperty("meta")] 
-        public TitlesMeta? Meta { get; set; }
-    }
+    [JsonProperty("meta")] 
+    public TitlesMeta? Meta { get; set; }
+}
 
-    public class TitlesMeta
-    {
-        [JsonProperty("date")] 
-        public DateTime Date { get; set; }
+public class TitlesMeta
+{
+    [JsonProperty("date")] 
+    public DateTime Date { get; set; }
 
-        [JsonProperty("import_in_progress")] 
-        public bool ImportInProgress { get; set; }
-    }
+    [JsonProperty("import_in_progress")] 
+    public bool ImportInProgress { get; set; }
 }

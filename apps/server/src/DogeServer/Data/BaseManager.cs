8a39fea15e0a8cda9b57819c360a32f2ae3e0d42
jacobvoller <act@jacobvoller.com>
2025-03-22 10:42:39 -0500

    IGNORE warnings

--- apps/server/src/DogeServer/Data/BaseManager.cs
+++ apps/server/src/DogeServer/Data/BaseManager.cs
@@ -62,6 +62,7 @@ namespace DogeServer.Data
         {
             Guid? createdEntityID = null;
 
+#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
             await ExecuteDatabaseQuery(async db =>
             {
                 var table = db.GetTable<T>();
@@ -69,6 +70,7 @@ namespace DogeServer.Data
 
                 return createdEntityID;
             });
+#pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
 
             return entity.ID;
         }

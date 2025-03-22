d463e27b968d1466694f45f7e9256f6daf2ef660
jacobvoller <act@jacobvoller.com>
2025-03-22 08:04:18 -0500

    ADD DogeServiceResponse

---
+++ apps/server/src/DogeServer/Models/DogeServiceResponse.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DogeServer.Models
+{
+    public class DogeServiceControllerResponse<T>
+    {
+        public T? Results { get; set; }
+        public string? Message { get; set; }
+        public int? StatusCode { get; set; }
+        public int? ErrorCode { get; set; }
+        public string? ErrorMessage { get; set; }
+        public string? StackTrace { get; set; }
+    }
+
+    public static class DogeServiceResponse
+    {
+        public static async Task<ObjectResult> GenerateControllerResponse<T>(Func<Task<DogeServiceControllerResponse<T>>> func)
+        {
+            try
+            {
+                var controllerResponse = await func();
+                controllerResponse = controllerResponse ?? new DogeServiceControllerResponse<T>();
+
+                var hasError = !string.IsNullOrEmpty(controllerResponse?.ErrorMessage);
+                return (hasError)
+                    ? ErrorResponse(controllerResponse)
+                    : new OkObjectResult(controllerResponse); // return HTTP StatusCode 200
+            }
+            catch (Exception exception)
+            {
+                var response = new DogeServiceControllerResponse<int>();
+                response.ErrorMessage = exception?.Message;
+                response.StackTrace = exception?.StackTrace;
+
+                return ErrorResponse<int>(response);
+            }
+        }
+
+        private static ObjectResult ErrorResponse<T>(DogeServiceControllerResponse<T> responseObject)
+        {
+            var httpStatusCode = responseObject?.StatusCode ?? StatusCodes.Status500InternalServerError;
+
+            return new ObjectResult(responseObject)
+            {
+                StatusCode = httpStatusCode
+            };
+        }
+    }
+}

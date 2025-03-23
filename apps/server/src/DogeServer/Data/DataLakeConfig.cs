cfe3e4dae5b9a8960e2d8a67911a9eef83ad9251
jacobvoller <act@jacobvoller.com>
2025-03-23 10:38:00 -0500

    LINT

--- apps/server/src/DogeServer/Data/DataLakeConfig.cs
+++ apps/server/src/DogeServer/Data/DataLakeConfig.cs
@@ -1,10 +1,9 @@
-﻿namespace DogeServer.Data
+﻿namespace DogeServer.Data;
+
+public class DataLakeConfig
 {
-    public class DataLakeConfig
-    {
-        public string? Host { get; set; }
-        public string? Database { get; set; }
-        public string? Username { get; set; }
-        public string? Password { get; set; }   
-    }
+    public string? Host { get; set; }
+    public string? Database { get; set; }
+    public string? Username { get; set; }
+    public string? Password { get; set; }   
 }

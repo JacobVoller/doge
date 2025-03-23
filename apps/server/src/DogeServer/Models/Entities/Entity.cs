13d14f9265edbadf97c20eb18d72bee6d7d8d2f7
jacobvoller <act@jacobvoller.com>
2025-03-23 10:37:47 -0500

    Move Entity.cs

---
+++ apps/server/src/DogeServer/Models/Entities/Entity.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace DogeServer.Models.Entities;
+
+public abstract class Entity
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public Guid? ID { get; set; }
+
+    public DateTime? Created { get; set; }
+    public DateTime? Deleted { get; set; }
+}

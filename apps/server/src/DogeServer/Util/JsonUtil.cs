ccbd9d53b44cc290d7207ffadda5b4749c0d60d1
jacobvoller <act@jacobvoller.com>
2025-03-23 10:38:21 -0500

    LINT

--- apps/server/src/DogeServer/Util/JsonUtil.cs
+++ apps/server/src/DogeServer/Util/JsonUtil.cs
@@ -1,46 +1,45 @@
 ﻿using Newtonsoft.Json;
 
-namespace DogeServer.Util
+namespace DogeServer.Util;
+
+public static class JsonUtil
 {
-    public static class JsonUtil
+    private static readonly JsonSerializerSettings Settings = new()
     {
-        private static readonly JsonSerializerSettings Settings = new()
-        {
-            Formatting = Formatting.Indented
-        };
+        Formatting = Formatting.Indented
+    };
 
-        public static string? Serialize(object? o)
+    public static string? Serialize(object? o)
+    {
+        return (o == null)
+            ? null
+            : JsonConvert.SerializeObject(o, Settings);
+    }
+
+    public static T? DeSerialize<T>(string? json)
+    {
+        T? invalid = default;
+
+        if (string.IsNullOrWhiteSpace(json))
+            return invalid;
+
+        if (json.Contains("\\\"")) // encoded
         {
-            return (o == null)
-                ? null
-                : JsonConvert.SerializeObject(o, Settings);
+            json = StringUtil.RemoveFirstChar(json);
+            json = StringUtil.RemoveLastChar(json);
+            json = StringUtil.EscapeBackslashes(json);
         }
 
-        public static T? DeSerialize<T>(string? json)
-        {
-            T? invalid = default;
+        return JsonConvert.DeserializeObject<T>(json);
+    }
 
-            if (string.IsNullOrWhiteSpace(json))
-                return invalid;
+    public static string? TrimQuotes(string? input)
+    {
+        if (input == null) return input;
 
-            if (json.Contains("\\\"")) // encoded
-            {
-                json = StringUtil.RemoveFirstChar(json);
-                json = StringUtil.RemoveLastChar(json);
-                json = StringUtil.EscapeBackslashes(json);
-            }
-
-            return JsonConvert.DeserializeObject<T>(json);
-        }
-
-        public static string? TrimQuotes(string? input)
-        {
-            if (input == null) return input;
-
-            var startsAndEndsWithQuote = input.Length >= 2 && input[0] == '"' && input[^1] == '"';
-            return startsAndEndsWithQuote
-                ? input.Substring(1, input.Length - 2)
-                : input;
-        }
+        var startsAndEndsWithQuote = input.Length >= 2 && input[0] == '"' && input[^1] == '"';
+        return startsAndEndsWithQuote
+            ? input.Substring(1, input.Length - 2)
+            : input;
     }
 }

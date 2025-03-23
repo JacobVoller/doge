ad8c960335ff1e7755ecbd6ceabbeb9bbe9632dc
jacobvoller <act@jacobvoller.com>
2025-03-23 16:50:57 -0500

    UPDATE server build script

--- apps/server/scripts/build.sh
+++ apps/server/scripts/build.sh
@@ -1,1 +1,13 @@
 echo "      > [SERVER] Build"
+
+DIR_SERVER_ARTIFACTS_LOCAL="published"
+DIR_SERVER_ARTIFACTS="../../../../ServerArtifacts"
+
+# BUILD
+cd apps/server/src/DogeServer/
+dir_delete "$DIR_SERVER_ARTIFACTS"
+dotnet publish -c Release -o published
+dir_move "$DIR_SERVER_ARTIFACTS_LOCAL" "$DIR_SERVER_ARTIFACTS"
+cd ../../../../
+
+echo "      > [SERVER] End"

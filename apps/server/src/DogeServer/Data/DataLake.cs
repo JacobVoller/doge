d014e5d12f78d374dee49a50be809ee42fca3be9
jacobvoller <act@jacobvoller.com>
2025-03-23 10:39:15 -0500

    LINT

--- apps/server/src/DogeServer/Data/DataLake.cs
+++ apps/server/src/DogeServer/Data/DataLake.cs
@@ -1,50 +1,49 @@
 ﻿using DogeServer.Data.Managers;
 using Microsoft.EntityFrameworkCore;
 
-namespace DogeServer.Data
+namespace DogeServer.Data;
+
+public class DataLake
 {
-    public class DataLake
+    public readonly OutlineManager Outline;
+
+    protected string DatabaseName { get; set; } = "doge"; //TODO
+    protected DbContextOptions<DatabaseContext> DatabaseOptions { get; set; }
+    //protected DatabaseContext DatabaseContext { get; set; } //TODO
+
+    public DataLake(bool useInMemoryDb = true)
     {
-        public readonly OutlineManager Outline;
+        //DatabaseContext = new DatabaseContext(); //TODO
+        DatabaseOptions = useInMemoryDb
+            ? ConfigureInMemoryDbOptions()
+            : ConfigurePostgresOptions();
 
-        protected string DatabaseName { get; set; } = "doge"; //TODO
-        protected DbContextOptions<DatabaseContext> DatabaseOptions { get; set; }
-        //protected DatabaseContext DatabaseContext { get; set; } //TODO
+        Outline = new(DatabaseConnection);
+    }
 
-        public DataLake(bool useInMemoryDb = true)
+    private DbContextOptions<DatabaseContext> ConfigureInMemoryDbOptions()
+    {
+        return new DbContextOptionsBuilder<DatabaseContext>()
+            .UseInMemoryDatabase(databaseName: DatabaseName)
+            .Options;
+    }
+
+    private DbContextOptions<DatabaseContext> ConfigurePostgresOptions()
+    {
+        var host = "doge"; //TODO
+        var username = "doge"; //TODO
+        var password = "doge"; //TODO
+
+        return new DbContextOptionsBuilder<DatabaseContext>()
+            .UseNpgsql($"Host={host};Database={DatabaseName};Username={username};Password={password};")
+            .Options;
+    }
+
+    private DatabaseContext DatabaseConnection()
+    {
+        return new DatabaseContext(DatabaseOptions)
         {
-            //DatabaseContext = new DatabaseContext(); //TODO
-            DatabaseOptions = useInMemoryDb
-                ? ConfigureInMemoryDbOptions()
-                : ConfigurePostgresOptions();
-
-            Outline = new(DatabaseConnection);
-        }
-
-        private DbContextOptions<DatabaseContext> ConfigureInMemoryDbOptions()
-        {
-            return new DbContextOptionsBuilder<DatabaseContext>()
-                .UseInMemoryDatabase(databaseName: DatabaseName)
-                .Options;
-        }
-
-        private DbContextOptions<DatabaseContext> ConfigurePostgresOptions()
-        {
-            var host = "doge"; //TODO
-            var username = "doge"; //TODO
-            var password = "doge"; //TODO
-
-            return new DbContextOptionsBuilder<DatabaseContext>()
-                .UseNpgsql($"Host={host};Database={DatabaseName};Username={username};Password={password};")
-                .Options;
-        }
-
-        private DatabaseContext DatabaseConnection()
-        {
-            return new DatabaseContext(DatabaseOptions)
-            {
-                DatabaseName = DatabaseName
-            };
-        }
+            DatabaseName = DatabaseName
+        };
     }
 }

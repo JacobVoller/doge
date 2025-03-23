2ddb2c2c65a947881174ebb175e48819844c2d6b
jacobvoller <act@jacobvoller.com>
2025-03-23 16:56:18 -0500

    FIX build script

--- apps/server/scripts/build.sh
+++ apps/server/scripts/build.sh
@@ -5,6 +5,7 @@ DIR_SERVER_ARTIFACTS="../../../../ServerArtifacts"
 
 # BUILD
 cd apps/server/src/DogeServer/
+dir_delete "$DIR_SERVER_ARTIFACTS_LOCAL"
 dir_delete "$DIR_SERVER_ARTIFACTS"
 dotnet publish -c Release -o published
 dir_move "$DIR_SERVER_ARTIFACTS_LOCAL" "$DIR_SERVER_ARTIFACTS"

ade9cbdcc0813474ea0e52dea0301010b4128bbf
jacobvoller <act@jacobvoller.com>
2025-03-23 10:38:09 -0500

    LINT

--- apps/server/src/DogeServer/Models/DogeServiceResponse.cs
+++ apps/server/src/DogeServer/Models/DogeServiceResponse.cs
@@ -1,12 +1,11 @@
 ﻿
-namespace DogeServer.Models
+namespace DogeServer.Models;
+
+public class DogeServiceControllerResponse<T>
 {
-    public class DogeServiceControllerResponse<T>
-    {
-        public T? Results { get; set; }
-        public int? StatusCode { get; set; }
-        public int? ErrorCode { get; set; }
-        public string? ErrorMessage { get; set; }
-        public string? StackTrace { get; set; }
-    }
+    public T? Results { get; set; }
+    public int? StatusCode { get; set; }
+    public int? ErrorCode { get; set; }
+    public string? ErrorMessage { get; set; }
+    public string? StackTrace { get; set; }
 }

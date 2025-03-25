c2540d1dde91e6f103a528e9589f2f41e55e71e5
jacobvoller <act@jacobvoller.com>
2025-03-24 20:22:12 -0500

    ADD XmlUtil

---
+++ apps/server/src/DogeServer/Util/XmlUtil.cs
@@ -0,0 +1,18 @@
+﻿using System.Xml.Serialization;
+
+namespace DogeServer.Util;
+
+public static class XmlUtil
+{
+    public static T? DeSerialize<T>(Stream? xmlStream)
+    {
+        if (xmlStream == null) return default;
+
+        var serializer = new XmlSerializer(typeof(T));
+        var obj = serializer.Deserialize(xmlStream);
+
+        return (obj == null)
+            ? default
+            : (T)obj;
+    }
+}

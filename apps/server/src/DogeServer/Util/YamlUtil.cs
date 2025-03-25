7278f20701dfd4e85c31b01cc9a5ffce6456a261
jacobvoller <act@jacobvoller.com>
2025-03-24 20:23:44 -0500

    ADD YamlUtil

---
+++ apps/server/src/DogeServer/Util/YamlUtil.cs
@@ -0,0 +1,35 @@
+﻿using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+
+namespace DogeServer.Util;
+
+public static class YamlUtil
+{
+    public static void Serialize<T>(T obj, string? filename)
+    {
+        const string dirName = "export";
+        
+        filename ??= StringUtil.Random();
+        filename += ".yml";
+
+        string dir = Path.Combine(Directory.GetCurrentDirectory(), dirName);
+        Directory.CreateDirectory(dir);
+
+        string filePath = Path.Combine(dir, filename);
+        if (File.Exists(filePath))
+        {
+            File.Delete(filePath);
+        }
+
+        var serializer = new SerializerBuilder()
+            .WithNamingConvention(CamelCaseNamingConvention.Instance)
+            .ConfigureDefaultValuesHandling(DefaultValuesHandling.OmitNull)
+            .ConfigureDefaultValuesHandling(DefaultValuesHandling.OmitDefaults)
+            .ConfigureDefaultValuesHandling(DefaultValuesHandling.OmitEmptyCollections)
+            .Build();
+
+        var yaml = serializer.Serialize(obj);
+
+        File.WriteAllText(filePath, yaml);
+    }
+}

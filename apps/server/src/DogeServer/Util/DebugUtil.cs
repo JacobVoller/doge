a19947c14bf147f8d1955b424c168ccae920ba46
jacobvoller <act@jacobvoller.com>
2025-03-26 12:22:14 -0500

    ADD DebugUtil

---
+++ apps/server/src/DogeServer/Util/DebugUtil.cs
@@ -0,0 +1,18 @@
+﻿namespace DogeServer.Util
+{
+    public static class DebugUtil
+    {
+        public static void Log(string operation, string status)
+        {
+            try
+            {
+                operation = operation.PadLeft(10, ' ');
+                status = (status + " ").PadRight(10, '-');
+                string line = new('-', 10);
+
+                Console.WriteLine($">  DogeServer ▸ [{operation}] {status}{line}");
+            }
+            catch { }
+        }
+    }
+}

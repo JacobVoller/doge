62ddab680e9da68e7bac02e77ea61044a309b11a
jacobvoller <act@jacobvoller.com>
2025-03-28 07:54:41 -0500

    ADD datalake.outline.GetOutlineByLevelAndLabel

--- apps/server/src/DogeServer/Data/Managers/OutlineManager.cs
+++ apps/server/src/DogeServer/Data/Managers/OutlineManager.cs
@@ -1,4 +1,5 @@
-﻿using DogeServer.Models.Entities;
+﻿using DogeServer.enums;
+using DogeServer.Models.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace DogeServer.Data.Managers;
@@ -45,7 +46,57 @@ public class OutlineManager(Func<DatabaseContext> dbConnectCallback)
         return [.. array];
     }
 
+    public async Task<Outline?> GetOutlineByLevelAndLabel(Level level, string? keyword, Guid? parentId = null)
+    {
+        if (string.IsNullOrEmpty(keyword)) return default;
+
+        var typeIdentifier = EnumUtil.Value(level);
+        keyword = keyword.ToLower().Trim();
+
+        var array = await ExecuteDatabaseQuery(async db =>
+        {
+            if (db == null)
+                return [];
+
+            var table = db.GetTable<Outline>();
+            if (table == null)
+                return [];
+
+            var results = table
+                .Where(outline => outline.Removed != true)
+                .Where(outline => outline.Deleted == null)
+                .Where(outline =>
+                    outline.Type != null
+                    && outline.Type.Equals(typeIdentifier, StringComparison.CurrentCultureIgnoreCase));
+
+            if (parentId != null)
+            {
+                results = results.Where(outline =>
+                    outline.ParentID == parentId);
+            }
 
 
+            if (level == Level.Title || level == Level.Chapter)
+            {
+                results = results.Where(outline =>
+                    outline.LabelLevel != null
+                    && outline.LabelLevel.Equals(keyword, StringComparison.CurrentCultureIgnoreCase));
+            }
+
+            //if (level == Level.Chapter)
+            //{
+            //    results = results.Where(outline =>
+            //        outline.LabelLevel != null
+            //        && outline.LabelLevel.Equals(keyword, StringComparison.CurrentCultureIgnoreCase));
+            //}
+
+            return await results.ToArrayAsync();
+        });
+
+        if (array == null) return default;
+        if (array.Length == 0) return default;
+
+        return array.FirstOrDefault();
+    }
 
 }

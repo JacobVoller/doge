6570d6f083d661cda05649f9bb233b5625438d49
jacobvoller <act@jacobvoller.com>
2025-03-29 08:43:49 -0500

    ADD Server unit tests

---
+++ apps/server/src/DogeServer.Tests/Services/XmlNodeProcessor.Tests.cs
@@ -0,0 +1,20 @@
+﻿using DogeServer.Services;
+using NUnit.Framework;
+
+using Assert = NUnit.Framework.Assert;
+
+namespace DogeServer.Tests.Services;
+
+public class XmlNodeProcessor_Tests
+{
+    [TestCase("", null)]
+    [TestCase("Chapter", null)]
+    [TestCase("Chapter I", "Chapter I")]
+    [TestCase(" Chapter II ", "Chapter II")]
+    [TestCase(" Chapter XIV [RESERVED]", "Chapter XIV")]
+    public void ParseHeaderInToExpectedLabelLevel(string? input, string? expected)
+    {
+        var actual = XmlNodeProcessor.ParseHeaderInToExpectedLabelLevel(input);
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+}

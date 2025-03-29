dca74355ad4b373b5dd3a86c82c8610165074501
jacobvoller <act@jacobvoller.com>
2025-03-29 08:59:30 -0500

    ADD tests for ParseHeaderInToExpectedLabelLevel

--- apps/server/src/DogeServer.Tests/Services/XmlNodeProcessor.Tests.cs
+++ apps/server/src/DogeServer.Tests/Services/XmlNodeProcessor.Tests.cs
@@ -1,16 +1,20 @@
 ﻿using DogeServer.Services;
+using DogeServer.Tests.NUnit;
 using NUnit.Framework;
 
 using Assert = NUnit.Framework.Assert;
 
 namespace DogeServer.Tests.Services;
 
-public class XmlNodeProcessor_Tests
+public class XmlNodeProcessorTests : BaseFixture
 {
     [TestCase("", null)]
     [TestCase("Chapter", null)]
+    [TestCase(" Chapter", null)]
+    [TestCase("Chapter ", null)]
     [TestCase("Chapter I", "Chapter I")]
-    [TestCase(" Chapter II ", "Chapter II")]
+    [TestCase(" Chapter II", "Chapter II")]
+    [TestCase("Chapter II ", "Chapter II")]
     [TestCase(" Chapter XIV [RESERVED]", "Chapter XIV")]
     public void ParseHeaderInToExpectedLabelLevel(string? input, string? expected)
     {

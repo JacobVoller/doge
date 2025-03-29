515e2173dd1a632bd9ca82348e4e56d7b57f769f
jacobvoller <act@jacobvoller.com>
2025-03-29 08:59:58 -0500

    ADD Server test on build script

--- apps/server/scripts/build.sh
+++ apps/server/scripts/build.sh
@@ -1,5 +1,6 @@
 echo "      > [SERVER] Build"
 
+# CONFIGURE
 DIR_SERVER_ARTIFACTS_LOCAL="published"
 DIR_SERVER_ARTIFACTS="../../../../ServerArtifacts"
 
@@ -9,6 +10,17 @@ dir_delete "$DIR_SERVER_ARTIFACTS_LOCAL"
 dir_delete "$DIR_SERVER_ARTIFACTS"
 dotnet publish -c Release -o published
 dir_move "$DIR_SERVER_ARTIFACTS_LOCAL" "$DIR_SERVER_ARTIFACTS"
+
+# TEST
+echo "      > [SERVER] Test"
+cd ../DogeServer.Tests/
+dotnet test /consoleLoggerParameters:ErrorsOnly \
+    --logger "console;verbosity=minimal;consoleLoggerParameters=ErrorsOnly;" \
+    --collect:"XPlat Code Coverage" \
+    --results-directory ../../../../TestResults
 cd ../../../../
+
+#TODO: export coverage
+#reportgenerator -reports:./TestResults/*/coverage.cobertura.xml -targetdir:./TestResults/CoverageReport -reporttypes:Html
 
 echo "      > [SERVER] End"
